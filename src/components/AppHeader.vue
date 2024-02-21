7a01c8aa2c68faf09ecde2ae50abd1f0337c2918
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-02-21 12:12:27 +0100

    links hovers and dropdown

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -23,7 +23,7 @@ export default {
         <img src="../assets/Logo.png" alt="">
       </a>
 
-      <ul class="nav nav-pills text-uppercase fw-bold ">
+      <ul class="nav nav-pills text-uppercase fw-bold d-flex justify-content-center align-items-center ">
         <li><a href="" class="nav-link">home</a></li>
         <li><a href="" class="nav-link">blog</a></li>
 
@@ -31,9 +31,11 @@ export default {
         <li class="dropdown">
           <a href="javascript:void(0)" class="dropbtn">events</a>
           <div class="dropdown-content">
-            <a href="#">Link 1</a>
-            <a href="#">Link 2</a>
-            <a href="#">Link 3</a>
+            <a href="#">choral music</a>
+            <a href="#">concert band</a>
+            <a href="#">opera concerts</a>
+            <a href="#">symphony orchestra</a>
+            <a href="#">family concerts</a>
           </div>
 
         </li>
@@ -53,7 +55,7 @@ export default {
 
         </li>
 
-        <li><a href="" class="nav-link">icon</a></li>
+        <li><a href="" class="nav-link"><img src="../assets/image (23).svg" alt=""></a></li>
       </ul>
 
     </header>
@@ -74,6 +76,10 @@ li a,
   text-decoration: none;
 }
 
+li a img {
+  filter: invert(100%) sepia(44%) saturate(2333%) hue-rotate(185deg) brightness(112%) contrast(100%);
+}
+
 li a:hover,
 .dropdown:hover .dropbtn {
   color: $link-hover-color;
@@ -84,20 +90,23 @@ li.dropdown {
 }
 
 .dropdown-content {
+
   display: none;
   position: absolute;
-  background-color: #f9f9f9;
+  background-color: #1F1F1F;
   min-width: 160px;
   box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
   z-index: 1;
+  right: 0px;
 }
 
 .dropdown-content a {
-  color: black;
+  color: white;
   padding: 12px 16px;
   text-decoration: none;
   display: block;
   text-align: left;
+  border-bottom: 1px solid white;
 }
 
 .dropdown-content a:hover {
